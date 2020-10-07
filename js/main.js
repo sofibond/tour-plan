eb98a713941f9e192180681e0b527627ebe6c115
sofibond <sofibond28@gmail.com>
2020-10-07 19:41:51 +0300

    create: modal

--- js/main.js
+++ js/main.js
@@ -1,47 +1,68 @@
-ymaps.ready(init);
-var myMap, myPlacemark;
+$(document).ready(function () {
+  ymaps.ready(init);
+  var myMap, myPlacemark;
 
-function init() {
-  myMap = new ymaps.Map("map", {
-    center: [7.890633, 98.294922],
-    zoom: 15
+  function init() {
+    myMap = new ymaps.Map("map", {
+      center: [7.890633, 98.294922],
+      zoom: 15
+    });
+
+    myPlacemark = new ymaps.Placemark([7.890633, 98.294922], {
+      hintContent: 'Grand Hilton Hotel',
+      balloonContent: 'Grand Hilton Hotel, Phuket'
+    });
+    myMap.geoObjects.add(myPlacemark);
+
+  }
+
+  var hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+  })
+
+  hotelSlider.keyboard.enable();
+
+  var reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
+  })
+  reviewsSlider.keyboard.enable();
+
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener('click', function () {
+    console.log('Клик по кнопке меню');
+    document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible');
   });
 
-  myPlacemark = new ymaps.Placemark([7.890633, 98.294922], {
-    hintContent: 'Grand Hilton Hotel',
-    balloonContent: 'Grand Hilton Hotel, Phuket'
-  });
-  myMap.geoObjects.add(myPlacemark);
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
 
-}
-
-var hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
+  function openModal () {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
 })
-
-hotelSlider.keyboard.enable();
-
-var reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
-})
-reviewsSlider.keyboard.enable();
-
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener('click', function (){
-  console.log('Клик по кнопке меню');
-  document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible');
-});
--- send.php
+++ send.php
@@ -24,7 +24,7 @@ try {
     $mail->isSMTP();   
     $mail->CharSet = "UTF-8";
     $mail->SMTPAuth   = true;
-    $mail->SMTPDebug = 2;
+    $mail->SMTPDebug = 0;
     $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
 
     // Настройки вашей почты
@@ -53,4 +53,4 @@ else {$result = "error";}
 }
 
 // Отображение результата
-echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
+header("Location: thankyou.html");
