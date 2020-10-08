5643789240801c060e05a29e6aa89cafd4f0b2bf
sofibond <sofibond28@gmail.com>
2020-10-08 17:49:25 +0300

    create: form validation

--- js/main.js
+++ js/main.js
@@ -65,4 +65,24 @@ $(document).ready(function () {
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   }
-})
+
+  //Обработка форм
+  $(".form").each(function() {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: "Must contain at least 2 characters"
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com"
+        },
+        phone: {
+          required: "Phone number is required"
+        }
+      }
+    });
+  });
+});
