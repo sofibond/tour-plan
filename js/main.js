756cfbe744e5092d766ce9a677324437f04c6607
sofibond <sofibond28@gmail.com>
2020-10-10 20:32:40 +0300

    edit: smartphone version

--- js/main.js
+++ js/main.js
@@ -1,33 +1,16 @@
 $(document).ready(function () {
-  ymaps.ready(init);
-  var myMap, myPlacemark;
-
-  function init() {
-    myMap = new ymaps.Map("map", {
-      center: [7.890633, 98.294922],
-      zoom: 15
-    });
-
-    myPlacemark = new ymaps.Placemark([7.890633, 98.294922], {
-      hintContent: 'Grand Hilton Hotel',
-      balloonContent: 'Grand Hilton Hotel, Phuket'
-    });
-    myMap.geoObjects.add(myPlacemark);
-
-  }
-
-  var hotelSlider = new Swiper('.hotel-slider', {
+var hotelSlider = new Swiper('.hotel-slider', {
     // Optional parameters
-    loop: true,
+  loop: true,
 
     // Navigation arrows
-    navigation: {
-      nextEl: '.hotel-slider__button--next',
-      prevEl: '.hotel-slider__button--prev',
+  navigation: {
+    nextEl: '.hotel-slider__button--next',
+    prevEl: '.hotel-slider__button--prev',
     },
-  })
+})
 
-  hotelSlider.keyboard.enable();
+hotelSlider.keyboard.enable();
 
   var reviewsSlider = new Swiper('.reviews-slider', {
     // Optional parameters
